930f0bf380435ddf0d56896cc390a4cd149cb3f6
David Franke <mirroar@gmail.com>
2013-01-06 14:32:04 +0100

    fixed an error in ReduceItemList concerning uniqueness
    updated optimizer plugin
    added more dummy functions to set table

--- modules/optimizerplugin.class.lua
+++ modules/optimizerplugin.class.lua
@@ -3,6 +3,8 @@ local addonName, ns, _ = ...
 local OptimizerPlugin = ns.class(ns.Plugin)
 ns.OptimizerPlugin = OptimizerPlugin
 
+local colorOrder = {"META", "RED", "YELLOW", "BLUE", "PRISMATIC", "COGWHEEL", "HYDRAULIC"}
+
 -- creates a new ImportPlugin object
 function OptimizerPlugin:Initialize()
     self:SetName("Gear Optimizer")
@@ -11,6 +13,7 @@ function OptimizerPlugin:Initialize()
     self:RegisterConfigPanel()
 
     self.itemButtons = {}
+    self.headerTexts = {}
 end
 
 function OptimizerPlugin:GetItemButton(i)
@@ -25,6 +28,17 @@ function OptimizerPlugin:GetItemButton(i)
     end
 
     return self.itemButtons[i]
+end
+
+function OptimizerPlugin:GetHeaderText(i)
+    while #(self.headerTexts) < i do
+        local panel = self:GetConfigPanel()
+        local header = panel:CreateFontString("$parentHeaderText"..i, "ARTWORK", "GameFontNormal")
+
+        tinsert(self.headerTexts, header)
+    end
+
+    return self.headerTexts[i]
 end
 
 function OptimizerPlugin:HideItemButtonsAfter(index)
@@ -43,11 +57,23 @@ function OptimizerPlugin:OnShow()
             if not bestGems[color] then bestGems[color] = {} end
 
             tinsert(bestGems[color], ns:GetCachedItem(gemID))
+
+            if color == "RED" or color == "BLUE" or color == "YELLOW" then
+                if not bestGems["PRISMATIC"] then bestGems["PRISMATIC"] = {} end
+                tinsert(bestGems["PRISMATIC"], ns:GetCachedItem(gemID))
+            end
         end
     end
 
-    --TODO: all uniquenesses are problematic for gems
     ns.ReduceGemList(set, bestGems)
+
+    -- sort gems by score, descending
+    for _, subTable in pairs(bestGems) do
+        table.sort(subTable, function(a, b)
+            local scoreA, scoreB = ns:GetItemScore(a.itemLink, ns.setCode, set.calculationData.ignoreCapsForCalculation), ns:GetItemScore(b.itemLink, ns.setCode, set.calculationData.ignoreCapsForCalculation)
+            return scoreB < scoreA
+        end)
+    end
 
     ns.debugGems = bestGems
 
@@ -61,7 +87,23 @@ function OptimizerPlugin:OnShow()
     -- show suggested gems
     local panel = self:GetConfigPanel()
     local i = 1
-    for color, subTable in pairs(bestGems) do
+    local headerNum = 1
+    local categoryButtonCounter
+    local width = panel:GetWidth()
+    ns:Print(width)
+    if not width or width == 0 then
+        width = 355
+    end
+    for _, color in ipairs(colorOrder) do
+        local subTable = bestGems[color]
+        local header = self:GetHeaderText(headerNum)
+        header:SetText(_G["EMPTY_SOCKET_"..color])
+        if headerNum == 1 then
+            header:SetPoint("TOPLEFT", panel, "TOPLEFT")
+        else
+            header:SetPoint("TOPLEFT", self:GetItemButton(i - 1 - (categoryButtonCounter % 2)), "BOTTOMLEFT", 0, -20)
+        end
+        categoryButtonCounter = 1
         for _, gemTable in pairs(subTable) do
             local itemButton = self:GetItemButton(i)
             local texture = select(10, GetItemInfo(gemTable.itemID))
@@ -70,17 +112,19 @@ function OptimizerPlugin:OnShow()
 
             itemButton.itemLink = gemTable.itemLink
 
-            if i == 1 then
-                itemButton:SetPoint("TOPLEFT", panel, "TOPLEFT")
-            elseif i == 2 then
-                itemButton:SetPoint("TOPLEFT", self:GetItemButton(i - 1), "TOPLEFT", panel:GetWidth() / 2, 0)
+            if categoryButtonCounter == 1 then
+                itemButton:SetPoint("TOPLEFT", header, "BOTTOMLEFT", 0, -10)
+            elseif categoryButtonCounter == 2 then
+                itemButton:SetPoint("TOPLEFT", self:GetItemButton(i - 1), "TOPLEFT", width / 2, 0)
             else
                 itemButton:SetPoint("TOPLEFT", self:GetItemButton(i - 2), "TOPLEFT", 0, -(itemButton:GetHeight() + 5))
             end
             itemButton:Show()
 
             i = i + 1
+            categoryButtonCounter = categoryButtonCounter + 1
         end
+        headerNum = headerNum + 1
     end
     self:HideItemButtonsAfter(i)
 end
