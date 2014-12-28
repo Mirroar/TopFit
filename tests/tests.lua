bc70e2b0487bcc0f891e540f9bfb3810d0522c47
David Franke <mirroar@gmail.com>
2014-12-28 16:12:53 +0100

    adding some tests

--- tests/tests.lua
+++ tests/tests.lua
@@ -6,9 +6,32 @@ ns.testsLoaded = true;
 ns.tests = {
 	["Utility Stuff"] = {
 		["GenerateSetName"] = function()
-			wowUnit:assertEquals(ns:GenerateSetName("Test"), "Test (TF)", "Simple set naming works")
-			wowUnit:assertEquals(ns:GenerateSetName("ReallyLongSetName"), "ReallyLongSe(TF)", "Long names get cropped")
+			wowUnit:assertEquals(ns:GenerateSetName("Test"), "Test (TF)", "Simple set naming works.")
+			wowUnit:assertEquals(ns:GenerateSetName("ReallyLongSetName"), "ReallyLongSe(TF)", "Long names get cropped.")
 		end
+	},
+	["Sets"] = {
+		["default values"] = function()
+			local set = ns.Set()
+
+			wowUnit:assertEquals(set:GetName(), "Unknown", "New sets are named 'Unknown'.")
+			wowUnit:assertEquals(set:GetEquipmentSetName(), "Unknown (TF)", "Sets have an appropriate equipment set name.")
+
+			wowUnit:isString(set:GetIconTexture(), "A default icon is provided.")
+			wowUnit:assert(set:GetIconTexture():sub(1, 16) == "Interface\\Icons\\", "Default icon has the correct texture path.")
+
+			wowUnit:isEmpty(set:GetHardCaps(), "A new set has no hard caps set.")
+			wowUnit:isTable(set:GetHardCaps(), "Set:GetHardCaps always returns a table.")
+			wowUnit:isNil(set:GetHardCap('FOO'), "Trying to get a non-existant cap returns nil.")
+
+			set = ns.Set("AName")
+			wowUnit:assertEquals(set:GetName(), "AName", "Constructor can take a set name.")
+		end,
+		["setting values and saved variables"] = function()
+			local vars = ns.Set.PrepareSavedVariableTable()
+
+			wowUnit:isTable(vars, "Prepared saved variables are a table of some sorts.")
+		end,
 	},
 	["Calculation"] = {
 		setup = function()
